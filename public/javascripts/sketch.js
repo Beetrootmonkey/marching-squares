240bb55878bf00a72d52d3b3d2ac8d147644d257
Beetrootmonkey <n.adelhard@googlemail.com>
2022-05-04 22:08:08 +0200

    Implemented chunks and cell merging/splitting

--- public/javascripts/sketch.js
+++ public/javascripts/sketch.js
@@ -7,15 +7,18 @@ let chunks = {};
 // Method to construct a key for 'cells' or 'chunks'
 const getKey = (x, y) => x + ' ' + y;
 
-// Cell size in pixels (width and height)
-let cellSize = 64;
+const sizeFactor = 2;
 
-// Chunk size in cells (width and height)
-// Big chunks means less chunks to draw, but a bigger image to render/reconstruct when modifying a chunk
-let chunkSize = 4
+// Cell size in pixels (width and height)
+const cellSize = 16 * sizeFactor;
+
+// Chunk size is in cells (width and height), but it's the exponent to 2.
+// Big chunks means less chunks to draw, but a bigger image to render/reconstruct when modifying a chunk.
+// 4 = 16x16, 3 = 8x8, 2 = 4x4, 1 = 2x2, 0 = 1x1 (same as one cell)
+const chunkSize = 4;
 
 // Scale of the OpenSimplexNoise
-let noiseScale = 10;
+const noiseScale = Math.floor(40 / sizeFactor);
 let openSimplex;
 
 // Amount of distinct values
@@ -33,10 +36,10 @@ const cameraPadding = 2;
 
 const mouseClickTypes = new Array(levels).fill(0).map((e, i) => i / (levels - 1));
 
-const pencilStrength = 0.5;
+const pencilStrength = 1;
 
 // Radius in cell size excluding the middle (0 is a single cell, 1 is 3x3 cells, 2 is 5x5, ...)
-const pencilSize = 2;
+const pencilSize = 4 / sizeFactor;
 
 // Which sample rate value to "place" on left-click, can have values of the mouseClickTypes array
 let mouseClickType = 0;
@@ -110,10 +113,18 @@ const edge = (canvas, gridPos, cornerA, cornerB, valueA, valueB) => {
 
 let cellRenderer;
 
+const cellRenderCache = {};
+
 const renderCell = (state, color, level, nwValue, neValue, seValue, swValue, gridPos, nw, ne, se, sw, n, e, s, w) => {
   // noStroke();
   // fill(color);
   //tint(color, color, color);
+
+  const cachedValue = cellRenderCache[state];
+
+  if (cachedValue) {
+    return cachedValue;
+  }
 
 
   if (!cellRenderer) {
@@ -273,8 +284,6 @@ const renderCell = (state, color, level, nwValue, neValue, seValue, swValue, gri
       canvas.endShape(CLOSE);
   }
 
-
-
   const img = createImage(canvas.width, canvas.height);
   img.copy(canvas, canvas.width / -2, canvas.height / -2, canvas.width, canvas.height, 0, 0, canvas.width, canvas.height);
 
@@ -287,6 +296,8 @@ const renderCell = (state, color, level, nwValue, neValue, seValue, swValue, gri
   // vtx(canvas, gridPos, sw);
   // canvas.endShape(CLOSE);
 
+  cellRenderCache[state] = img;
+
   return img;
 }
 
@@ -298,7 +309,7 @@ const sample = (value, level) => {
 
 const isWall = (value, level) => Math.floor(value * levels) >= level ? 1 : 0;
 
-const getNoiseValue = (x, y) => openSimplex.noise2D(x / noiseScale, y / noiseScale) * 0.5 + 0.5;
+const getNoiseValue = (x, y) => isWall(openSimplex.noise2D(x / noiseScale, y / noiseScale) * 0.5 + 0.5, 1);
 
 const getCell = (x, y) => cells[getKey(x, y)];
 
@@ -340,24 +351,212 @@ const createCell = (x, y) => {
   }
 
   const newCell = {
-    value: getNoiseValue(x, y)
+    gridX: x,
+    gridY: y,
+    value: getNoiseValue(x, y),
+    chunks: []
   };
   setCell(x, y, newCell);
 
   return newCell;
 }
 
-// Draws the saved image on the screen
-const drawChunk = (x, y) => {
-  // TODO: Implement
+const getChunk = (x, y) => chunks[getKey(x, y)];
+
+const setChunk = (x, y, chunk) => chunks[getKey(x, y)] = chunk;
+
+// gridX and gridY are the coordinates of the upper-left corner, size is the size in cells
+const createChildChunk = (gridX, gridY, size, parents) => {
+  const chunk = {
+    size,
+    gridX,
+    gridY,
+    image: null,
+    parents,
+    children: null,
+    cell: null,
+    timeout: null
+  };
+
+  if (!parents) {
+    parents = [];
+  }
+  parents = [chunk, ...parents];
+
+  // Chunks bigger than 1 (= 2x2) contain chunks half the size
+  if (size > 0) {
+    const childSize = size - 1;
+
+    // Number of cells the child chunks are wide and high
+    const childCellSize = Math.pow(2, childSize);
+
+    chunk.children = {};
+
+    const createChild = (x, y) => chunk.children[getKey(x, y)] = createChildChunk(gridX + childCellSize * x, gridY + childCellSize * y, childSize, parents);
+
+    createChild(0, 0);
+    createChild(1, 0);
+    createChild(1, 1);
+    createChild(0, 1);
+  } else {
+    // If the chunk is 1x1, then it will contain a single cell instead of 4 chunks
+    chunk.cell = createCell(gridX, gridY);
+    chunk.cell.chunks = parents;
+    loadCellImage(gridX, gridY);
+    tryRerenderChunkRecursive(chunk);
+  }
+
+  return chunk;
+}
+
+// Gets or creates and gets the chunk at the given coords. x and y are in chunk coords
+const createChunk = (x, y) => {
+  const chunk = getChunk(x, y);
+
+  if (chunk) {
+    return chunk;
+  }
+
+  const chunkCellSize = Math.pow(2, chunkSize);
+  const newChunk = createChildChunk(x * chunkCellSize, y * chunkCellSize, chunkSize);
+  setChunk(x, y, newChunk);
+
+  return newChunk;
+}
+
+let chunkRenderer;
+
+// x and y are in screen coords
+const drawChunk = (chunk, x, y) => {
+  if (!chunk) {
+    return;
+  }
+
+  const chunkCellSize = Math.pow(2, chunk.size);
+
+  push();
+
+  if (chunk.timeout) {
+    stroke('red');
+    strokeWeight((chunk.size + 1) * 2);
+  } else {
+    stroke('green');
+    strokeWeight(chunk.size + 1);
+  }
+  noFill();
+
+  rect(x, y, chunkCellSize * cellSize, chunkCellSize * cellSize);
+  pop();
+
+  if (chunk.image) {
+    image(chunk.image, x, y);
+  } else if (chunk.size > 0) {
+    Object.values(chunk.children).forEach((child) => {
+      // TODO: Das kann ruhig am Start der Methode passieren statt außerhalb
+      const coords = createVector(child.gridX, child.gridY).mult(cellSize).add(viewport.offset)
+      drawChunk(child, coords.x, coords.y);
+    });
+  } else {
+    const coords = createVector(chunk.gridX, chunk.gridY).mult(cellSize).add(viewport.offset);
+    const img = chunk.cell?.image;
+
+    if (img) {
+      image(img, coords.x, coords.y);
+    }
+  }
 };
 
 // TODO: Maybe have a slight delay in case more cells in the same chunk get edited
 // TODO: Maybe make it so chunks combine into bigger and bigger chunks over time and split down into small chunks and smaller when you edit one or more cells
 // Renders all the cells in a chunk to an image and saves it
-const renderChunk = (x, y) => {
-  // TODO: Implement
+const renderChunk = (chunk, force) => {
+  clearTimeout(chunk.timeout);
+  chunk.timeout = null;
+
+  if (chunk.parents) {
+    for (let parent of chunk.parents) {
+      if (parent.timeout) {
+        clearTimeout(parent.timeout);
+        parent.timeout = null;
+      }
+    }
+  }
+
+  if (chunk.image) {
+    // If we already have an image, just return that 
+    return chunk.image;
+  } else if (chunk.size > 0) {
+    // Render all child chunks and stitch their image together
+    const size = Math.pow(2, chunk.size) * cellSize;
+    const img = createImage(size, size);
+
+    for (let child of Object.values(chunk.children)) {
+      const coords = createVector(child.gridX - chunk.gridX, child.gridY - chunk.gridY).mult(cellSize)
+
+      let childImage;
+      if (force) {
+        childImage = renderChunk(child, true);
+      } else {
+        childImage = child.image;
+      }
+
+      // Abort if we have a child chunk that has no image yet
+      if (!force && !childImage) {
+        return null;
+      }
+
+      if (childImage) {
+        img.copy(childImage, 0, 0, childImage.width, childImage.height, coords.x, coords.y, childImage.width, childImage.height);
+      }
+    };
+
+    chunk.image = img;
+    return img;
+  } else {
+    // If it's a 1x1 chunk, just return its cell's image (if there is one)
+    const img = chunk.cell?.image;
+
+    if (img) {
+      chunk.image = img;
+    }
+    return chunk.image;
+  }
 };
+
+// x and y are in grid coords
+const loadCellImage = (x, y) => {
+  const l = 1;
+  createCell(x, y);
+  createCell(x + 1, y);
+  createCell(x + 1, y + 1);
+  createCell(x, y + 1);
+
+  const gridPos = createVector(x, y);
+
+  if (!getCell(x,y).image) {
+    const nwValue = sample(getCellValue(x, y), l);
+    const neValue = sample(getCellValue(x + 1, y), l);
+    const seValue = sample(getCellValue(x + 1, y + 1), l);
+    const swValue = sample(getCellValue(x, y + 1), l);
+
+    const state = isWall(getCellValue(x, y), l) * 8
+      + isWall(getCellValue(x + 1, y), l) * 4
+      + isWall(getCellValue(x + 1, y + 1), l) * 2
+      + isWall(getCellValue(x, y + 1), l);
+
+    const nw = createVector(0, 0);
+    const n = createVector(0.5, 0);
+    const ne = createVector(1, 0);
+    const e = createVector(1, 0.5);
+    const se = createVector(1, 1);
+    const s = createVector(0.5, 1);
+    const sw = createVector(0, 1);
+    const w = createVector(0, 0.5);
+
+    const img = renderCell(state, l / (levels - 1) * 255, l, nwValue, neValue, seValue, swValue, gridPos, nw, ne, se, sw, n, e, s, w);
+    setCellImage(x, y, img);
+  }
+}
 
 let wallTexture;
 let floorTexture;
@@ -368,11 +567,11 @@ function preload() {
 }
 
 function setup() {
-  frameRate(60);
+  frameRate(30);
   createCanvas(windowWidth, windowHeight, WEBGL);
 
-  floorTexture = scaleImage(floorTexture, 4);
-  wallTexture = scaleImage(wallTexture, 4);
+  floorTexture = scaleImage(floorTexture, sizeFactor);
+  wallTexture = scaleImage(wallTexture, sizeFactor);
 
   // noSmooth();
   // openSimplex = openSimplexNoise(random(42));
@@ -380,7 +579,7 @@ function setup() {
 
   screenToWorld = (x, y) => createVector(x, y).sub(viewport.offset);
   worldToScreen = (x, y) => createVector(x, y).add(viewport.offset);
-  worldToGrid = (x, y) => createVector(Math.floor(x / cellSize), Math.floor(y / cellSize));
+  worldToGrid = (x, y) => createVector(Math.round(x / cellSize), Math.round(y / cellSize));
   screenToGrid = (x, y) => {
     const world = screenToWorld(x, y);
     return worldToGrid(world.x, world.y);
@@ -394,29 +593,86 @@ function setup() {
 
 // x and y are in screen coords
 const applyPencil = (x, y) => {
-  const worldPos = screenToWorld(x, y);
-  const gridPos = worldToGrid(worldPos.x, worldPos.y);
-  const maxDistance = pencilSize * cellSize;
+  const gridPos = screenToGrid(x, y);
+  const worldPos = gridToWorld(gridPos.x, gridPos.y);
+  const maxDistance = (pencilSize + 0.5) * cellSize;
+
+  const changed = [];
 
   for (let j = -pencilSize; j <= pencilSize; j++) {
     for (let i = -pencilSize; i <= pencilSize; i++) {
       const cellGridPos = gridPos.copy().add(createVector(i, j));
       const cellWorldPos = gridToWorld(cellGridPos.x, cellGridPos.y);
       const distance = dist(cellWorldPos.x, cellWorldPos.y, worldPos.x, worldPos.y);
-      const distanceFactor = clamp(distance / maxDistance, 0, 1);
-      const inverseDistanceFactor = 1 - distanceFactor;
-      const strengthFactor = maxDistance > 0 ? inverseDistanceFactor : 1;
-      const strength = pencilStrength * strengthFactor;
 
-      modifyCell(cellGridPos.x, cellGridPos.y, strength);
+      if (distance > maxDistance) {
+        continue;
+      }
+
+      // const distanceFactor = clamp(distance / maxDistance, 0, 1);
+      // const inverseDistanceFactor = 1 - distanceFactor;
+      // const strengthFactor = maxDistance > 0 ? inverseDistanceFactor : 1;
+      // const strength = pencilStrength * strengthFactor;
+
+      changed.push(...modifyCell(cellGridPos.x, cellGridPos.y, pencilStrength));
     }
   }
+
+  for (let cell of new Set(changed)) {
+    const {gridX, gridY} = cell;
+    setCellImage(gridX, gridY, null);
+    loadCellImage(gridX, gridY);
+    for (let chunk of cell.chunks) {
+      chunk.image = null;
+    }
+    tryRerenderChunkRecursive(cell.chunks[0], 300);
+  }
+}
+
+const tryRerenderChunkRecursive = (chunk, delay) => {
+  if (chunk.timeout) {
+    clearTimeout(chunk.timeout);
+  }
+
+  if (chunk.parents) {
+    for (let parent of chunk.parents) {
+      if (parent.timeout) {
+        clearTimeout(parent.timeout);
+        parent.timeout = null;
+      }
+    }
+  }
+
+  chunk.timeout = setTimeout(() => {
+    chunk.timeout = null;
+    const img = renderChunk(chunk, false);
+    if (img && chunk.parents) {
+      const parent = chunk.parents[0];
+      tryRerenderChunkRecursive(parent, delay);
+    }
+  }, delay || Math.random() * 100);
 }
 
 // x and y are in grid coords
 const modifyCell = (x, y, strength) => {
-  setCellValue(x, y, lerp(getCellValue(x, y), mouseClickType, strength));
-  setCellImage(x, y, null);
+  const oldValue = getCellValue(x, y);
+  const newValue = mouseClickType;
+
+  if (oldValue === newValue) {
+    return [];
+  }
+
+  setCellValue(x, y, newValue);
+
+  const changed = [];
+
+  for (let j = -1; j < 1; j++) {
+    for (let i = -1; i < 1; i++) {
+      changed.push(getCell(x + i, y + j));
+    }
+  }
+
+  return changed;
 }
 
 const increaseMouseClickType = (increase) => {
@@ -491,6 +747,14 @@ function draw() {
   viewport.gridY = Math.floor(viewport.y / cellSize);
   viewport.gridWidth = Math.ceil(width / cellSize);
   viewport.gridHeight = Math.ceil(height / cellSize);
+
+
+  const chunkCellSize = Math.pow(2, chunkSize);
+  viewport.chunkX = Math.floor(viewport.x / cellSize / chunkCellSize);
+  viewport.chunkY = Math.floor(viewport.y / cellSize / chunkCellSize);
+  viewport.chunkWidth = Math.ceil(width / cellSize / chunkCellSize);
+  viewport.chunkHeight = Math.ceil(height / cellSize / chunkCellSize);
+
   viewport.offset = createVector(viewport.x, viewport.y).mult(-1);
 
   if (mouseDown) {
@@ -499,43 +763,28 @@ function draw() {
 
   background(80);
   // for(let l = 0; l < levels; l++) {
-  const l = 1;
-  for(let j = viewport.gridY - cameraPadding; j < viewport.gridY + viewport.gridHeight + cameraPadding; j++) {
-    for(let i = viewport.gridX - cameraPadding; i < viewport.gridX + viewport.gridWidth + cameraPadding; i++) {
+  // const l = 1;
+  // for(let j = viewport.gridY - cameraPadding; j < viewport.gridY + viewport.gridHeight + cameraPadding; j++) {
+  //   for(let i = viewport.gridX - cameraPadding; i < viewport.gridX + viewport.gridWidth + cameraPadding; i++) {
+  //     loadCellImage(i, j);
+  //   }
+  // }
 
-      createCell(i, j);
-      createCell(i + 1, j);
-      createCell(i + 1, j + 1);
-      createCell(i, j + 1);
+  // for(let j = viewport.chunkY; j < viewport.chunkY + viewport.chunkHeight + 1; j++) {
+  //   for(let i = viewport.chunkX; i < viewport.chunkX + viewport.chunkWidth + 1; i++) {
+  //     const chunk = createChunk(i, j);
+  //     renderChunk(chunk);
+  //   }
+  // }
 
-      const gridPos = createVector(i, j);
+  for(let j = viewport.chunkY; j < viewport.chunkY + viewport.chunkHeight + 1; j++) {
+    for(let i = viewport.chunkX; i < viewport.chunkX + viewport.chunkWidth + 1; i++) {
 
-      if (!getCell(i,j).image) {
-        const nwValue = sample(getCellValue(i, j), l);
-        const neValue = sample(getCellValue(i + 1, j), l);
-        const seValue = sample(getCellValue(i + 1, j + 1), l);
-        const swValue = sample(getCellValue(i, j + 1), l);
+      const chunk = createChunk(i, j);
 
-        const state = isWall(getCellValue(i, j), l) * 8
-          + isWall(getCellValue(i + 1, j), l) * 4
-          + isWall(getCellValue(i + 1, j + 1), l) * 2
-          + isWall(getCellValue(i, j + 1), l);
+      const coords = createVector(chunk.gridX, chunk.gridY).mult(cellSize).add(viewport.offset)
 
-        const nw = createVector(0, 0);
-        const n = createVector(0.5, 0);
-        const ne = createVector(1, 0);
-        const e = createVector(1, 0.5);
-        const se = createVector(1, 1);
-        const s = createVector(0.5, 1);
-        const sw = createVector(0, 1);
-        const w = createVector(0, 0.5);
-
-        const img = renderCell(state, l / (levels - 1) * 255, l, nwValue, neValue, seValue, swValue, gridPos, nw, ne, se, sw, n, e, s, w);
-        setCellImage(i, j, img);
-      }
-
-      const coords = gridPos.copy().mult(cellSize).add(viewport.offset);
-      image(getCellImage(i, j), coords.x, coords.y);
+      drawChunk(chunk, coords.x, coords.y);
     }
   }
 
@@ -557,7 +806,5 @@ function draw() {
   stroke('white');
   strokeWeight(1);
   noFill();
-  circle(mouseX, mouseY, (pencilSize + 1) * cellSize);
-
-  rect(mScreen.x, mScreen.y, cellSize, cellSize);
+  circle(mScreen.x, mScreen.y, (pencilSize * 2 + 1) * cellSize);
 }
